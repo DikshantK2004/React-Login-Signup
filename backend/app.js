00e4be3ea8de18b161b79fbdb355bfa607ecbeaa
Dikshant <dikkpsd@gmail.com>
2023-09-19 12:27:23 +0530

    Completed Backend

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,46 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const User = require('./models/user')
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const User = require("./models/user");
 
 const link = "mongodb+srv://dikkpsd:zksurt@users.s1qlvfq.mongodb.net/";
 
-const app = express()
+const app = express();
 app.use(cors()); // for sending data from frontend to backend
 // app.options('*', cors())
-app.use(express.json());                   // data using stringified json format
-app.use(bodyParser.urlencoded({extended:true}));  // helps with sending requests in form-encode part
+app.use(express.json()); // data using stringified json format
+app.use(bodyParser.urlencoded({ extended: true })); // helps with sending requests in form-encode part
 
-
-mongoose.connect(link).then(()=>{
+mongoose.connect(link).then(() => {
   console.log("connection is ready");
 });
-const port = 3001
+const port = 3001;
 
+app.get("/", async (req, res) => {
+  const userList = await User.find();
+  res.send(userList);
+});
 
-app.post('/', async (req, res) => {
-  let {email, username, password} = req.body;
-  console.log(username);
-    let user = new User(
-      {
-          email: req.body.email,
-          username: req.body.username,
-          password: req.body.password,
-      }
-    );
-    console.log(user);
-    user = await user.save();
-    if (!user) {
+app.post("/signup", async (req, res) => {
+  const data = {
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  let user = await User.findOne({ email: data.email });
+  console.log(user);
+  if (user) {
+    res.send({
+      alert: false,
+      message: "User Already exists",
+    });
+  } else {
+    let user_save = new User(data);
+    user_save = await user_save.save();
+    if (!user_save) {
       return res.send({
         message: "Can't Sign in",
         alert: false,
@@ -42,24 +50,28 @@ app.post('/', async (req, res) => {
       message: "Signup Successful",
       alert: true,
     });
-
+  }
 });
 
+app.get("/login/:email", async (req, res) => {
+  const email = req.params.email;
 
+  const user = await User.findOne({ email: email });
 
-app.get('/', async(req, res) => {
-  const userList = User.find();
-  res.send(userList);
+  if (user) {
+    res.send({
+      alert: true,
+      message: "User Exists",
+      data: user,
+    });
+  } else {
+    return res.send({
+      message: "Please Sign up",
+      alert: false,
+    });
+  }
 });
-
-app.post('/data', (req, res)=>{
-    const email = req.body.email;
-    const pass = req.body.email;
-    console.log(req.body);
-    res.send('success');
-});
-
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
